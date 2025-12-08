4bd350bcf45d005fd339350d27c116a370e4f4ba
mike kwatog <mike@kwatog.com>
2025-12-08 08:24:01 +0800

    refactor(frontend): restructure directory for Nuxt 4

--- frontend/middleware/auth.global.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const token = useCookie('access_token')
-
-  if (!token.value && to.path !== '/login') {
-    return navigateTo('/login')
-  }
-})
